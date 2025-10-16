1ae10358c315c8397d60ade97e0f09cac5ba2653
Maria Laura <mlsilmeida@gmail.com>
2025-10-16 20:48:36 -0300

    Create SalarioLiquido.java

---
+++ Lista EPS 2/SalarioLiquido.java
@@ -0,0 +1,34 @@
+package atividadestpa;
+import java.util.Scanner;
+public class SalarioLiquido {
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+			 //declaração de variáveis
+			 double valorHora, numeroAulas, inss, salarioBruto, desconto, salarioLiquido;
+			 
+		    
+			 System.out.println("Digite o valor por horas de cada aula: ");
+			 valorHora = ler.nextDouble();
+			 
+			 System.out.println("Digite o número de aulas dadas no mês: ");
+			 numeroAulas = ler.nextDouble();
+			 
+			 System.out.println("Digite o percentual de desconto do inss: ");
+			 inss = ler.nextDouble();
+			 
+			 
+             salarioBruto = valorHora*numeroAulas;
+	
+             desconto = salarioBruto* (inss/100);
+			
+			 salarioLiquido = salarioBruto - desconto;
+			 
+		
+			 System.out.println("O seu salário bruto é de: "+ salarioBruto);
+			 System.out.println("O seu salário líquido é de: "+ salarioLiquido);
+			  
+	
+
+	}
+
+}
