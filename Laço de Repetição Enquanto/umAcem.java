b92d8527347f36c065b0c9372ea5fcb4085b429a
Maria Laura <mlsilmeida@gmail.com>
2025-10-16 22:29:48 -0300

    Create umAcem.java

---
+++ Laço de Repetição Enquanto/umAcem.java
@@ -0,0 +1,19 @@
+package atividadestpa;
+import java.util.*;
+public class umAcem {
+		public static void main(String[] args) {
+			int i =1;
+			
+			while(i<=100) {
+				
+				System.out.println(i);
+				
+				i++;
+				
+				
+			}
+			
+	         
+		}
+
+	}
