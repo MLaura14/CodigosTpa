f0f07e307807e323b3fef702c85de278326818a7
Maria Laura <mlsilmeida@gmail.com>
2025-10-16 21:50:59 -0300

    Create MaiorDeIdade.java

---
+++ Tomada de Decisão 1/MaiorDeIdade.java
@@ -0,0 +1,31 @@
+package atividadestpa;
+import java.util.Scanner;
+public class anoNascimento {
+	public static void main(String[] args) {		 
+			Scanner ler = new Scanner(System.in);
+			int anoAtual, anoNascimento, idade;
+			 
+			System.out.println("Escreva o ano atual: ");
+			anoAtual = ler.nextInt();
+			 
+			System.out.println("Escreva o seu ano de nascimento: ");
+			anoNascimento = ler.nextInt();
+			 
+			idade = anoAtual-anoNascimento;
+			 
+			System.out.println("A sua idade é: " + idade);
+			 
+			if (idade < 18) {
+			 
+			 System.out.println("Menor de idade, não pode dirigir");
+			 
+			} else {
+			 
+			 System.out.println("Maior de idade, pode dirigir");
+			 
+
+
+	}
+
+  }
+}
