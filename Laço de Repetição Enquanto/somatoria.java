7e9be0b16d0a98a44ef4eee57f933f9ceddc31a6
Maria Laura <mlsilmeida@gmail.com>
2025-10-16 22:21:02 -0300

    Create somatoria.java

---
+++ Laço de Repetição Enquanto/somatoria.java
@@ -0,0 +1,19 @@
+package atividadestpa;
+import java.util.*;
+public class somatoria {
+	public static void main(String[] args) {
+		int i =1, soma = 0;
+		
+		while(i<101) {
+			
+			soma = soma + i;
+			
+			i++;
+			
+			
+		}
+		
+           System.out.println("A soma de 1 até 100 é: "+soma);
+	}
+
+}
