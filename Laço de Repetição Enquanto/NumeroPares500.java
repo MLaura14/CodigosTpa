d6f32e6903bb945e9111e522b5b129b435a7b750
Maria Laura <mlsilmeida@gmail.com>
2025-10-16 22:33:45 -0300

    Create NumeroPares500.java

---
+++ Laço de Repetição Enquanto/NumeroPares500.java
@@ -0,0 +1,18 @@
+package atividadestpa;
+import java.util.*;
+public class NumerosParesCem {
+	public static void main(String[] args) {
+		int i = 1;
+		
+		while (i <= 500) {
+			if (i % 2 == 0) {
+				System.out.print(i + "");
+			
+			}
+			i++;
+			
+		}
+
+	}
+
+}
